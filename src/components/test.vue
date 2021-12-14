09ee9674509fe7f5213b815bfb5850c6e2b0e67a
Breezy <1147159386@qq.com>
2021-12-14 14:19:49 +0800

    > import

---
+++ src/components/test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+ import HelloWorld from '@/components/HelloWorld.vue'
+export default {
+ 
+  setup () {
+    
+
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
