317e5512d231b7d05f1eef3fb1bc87fde42eb45f
Breezy <1147159386@qq.com>
2021-12-15 16:48:28 +0800

    money模块化

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,32 @@
+<template>
+  <label class="notes">
+    <span class="name">备注</span>
+    <input id="xxx" type="text" placeholder="输入备注" />
+  </label>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Notes",
+};
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  display: flex;
+  padding-left: 16px;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    border: none;
+    background: transparent;
+    height: 64px;
+    flex-grow: 1;
+    padding-right: 16px;
+  }
+}
+</style>
---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="tags">
+    <div class="new">
+      <button>新增</button>
+    </div>
+    <ul class="current">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+  </div>
+</template>
+
+<script lang='ts'>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  flex-grow: 1;
+  font-size: 14px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column-reverse;
+  > .current {
+    display: flex;
+    > li {
+      background: rgb(192, 185, 185);
+      height: 24px;
+      line-height: 24px;
+      border-radius: 12px;
+      padding: 0 16px;
+      margin-right: 16px;
+    }
+  }
+  > .new {
+    padding: 16px 0;
+    button {
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+      padding: 0 4px;
+    }
+  }
+}
+</style>
