5757e8a03b0b40896d37f2307299ef9cb03ae092
Breezy <1147159386@qq.com>
2021-12-16 20:23:55 +0800

    标签页路由

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout>
     <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+      <router-link class="tag" :to="`/labels/edit/${tag.id}`" v-for="tag in tags" :key="tag.id">
         <span>{{ tag.name }}</span> <Icon name="right" />
-      </li>
+      </router-link>
     </ol>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
@@ -40,7 +40,7 @@ export default class Labels extends Vue {
   background: white;
   font-size: 16px;
   padding-left: 16px;
-  > li {
+  > .tag {
     min-height: 44px;
     display: flex;
     align-items: center;
