8a1c2fa63c4b195d29395ce6142c4c275f1dabc0
Breezy <1147159386@qq.com>
2021-12-20 14:52:32 +0800

    抽离 TagHelper 作为 mixin

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button @click="create">新增</button>
+      <button @click="createTag">新增</button>
     </div>
     <ul class="current">
       <li
@@ -18,8 +18,9 @@
 
 <script lang="ts">
 import Vue from "vue";
-
 import { Component, Prop } from "vue-property-decorator";
+import { mixins } from "vue-class-component";
+import TagHelper from "@/mixins/TagHelper";
 @Component({
   computed: {
     tagList() {
@@ -27,7 +28,7 @@ import { Component, Prop } from "vue-property-decorator";
     },
   },
 })
-export default class Tags extends Vue {
+export default class Tags extends mixins(TagHelper) {
   @Prop(Array) dataSource: string[] | undefined;
   selectedTags: string[] = [];
   created() {
@@ -41,13 +42,6 @@ export default class Tags extends Vue {
       this.selectedTags.push(tag);
     }
     this.$emit("update:value", this.selectedTags);
-  }
-  create() {
-    const name = window.prompt("请输入标签名");
-    if (!name) {
-      return window.alert("标签名不能为空");
-    }
-    this.$store.commit("createTag", name);
   }
 }
 </script>
